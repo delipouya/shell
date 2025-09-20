b9a89f3190396c7caa618c80cb8eef29e9c2686b
delipouya <d.pouyabahar@mail.utoronto.ca>
2025-09-20 18:56:49 -0400

    adding a comment as a test

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -51,6 +51,7 @@ rm data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls data/processed -type f > data/inventory.txt
+# end of assignment
 
 ###########################################
 
